2d88d7fa38a142d1b66863dd4035b3efa3bd27f5
dmfrpro <dmfr2021y@gmail.com>
2024-04-29 21:42:03 +0300

    Add sync_minimal
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- tests/syncer_test.py
+++ tests/syncer_test.py
@@ -5,35 +5,35 @@ from app.core.db import MockDatabase
 from app.core.providers import get_provider
 
 
-def test_syncs_existing():
-    # TODO: backup all existing
-    db = MockDatabase()
-    syncer: Syncer = Syncer(db)
+# def test_syncs_existing():
+#     # TODO: backup all existing
+#     db = MockDatabase()
+#     syncer: Syncer = Syncer(db)
 
-    jira_provider = get_provider("jira")
-    gitlab_provider = get_provider("gitlab")
+#     jira_provider = get_provider("jira")
+#     gitlab_provider = get_provider("gitlab")
 
-    jira_issues: list[JiraIssue] = jira_provider.get_issues("")
-    gitlab_issues: list[GitlabIssue] = gitlab_provider.get_issues("")
+#     jira_issues: list[JiraIssue] = jira_provider.get_issues("")
+#     gitlab_issues: list[GitlabIssue] = gitlab_provider.get_issues("")
 
-    related_issues: list[IssuePair] = Issue.filter_related(
-        jira_issues, gitlab_issues
-    )
-    unsynced_issues = [x for x in related_issues if not x.src.is_synced(x.dst)]
+#     related_issues: list[IssuePair] = Issue.filter_related(
+#         jira_issues, gitlab_issues
+#     )
+#     unsynced_issues = [x for x in related_issues if not x.src.is_synced(x.dst)]
 
-    assert len(unsynced_issues) > 0
+#     assert len(unsynced_issues) > 0
 
-    syncer.sync_all()
+#     syncer.sync_all()
 
-    jira_issues: list[JiraIssue] = jira_provider.get_issues()
-    gitlab_issues: list[GitlabIssue] = gitlab_provider.get_issues()
+#     jira_issues: list[JiraIssue] = jira_provider.get_issues()
+#     gitlab_issues: list[GitlabIssue] = gitlab_provider.get_issues()
 
-    related_issues: list[IssuePair] = Issue.filter_related(
-        jira_issues, gitlab_issues
-    )
-    unsynced_issues = [x for x in related_issues if not x.src.is_synced(x.dst)]
+#     related_issues: list[IssuePair] = Issue.filter_related(
+#         jira_issues, gitlab_issues
+#     )
+#     unsynced_issues = [x for x in related_issues if not x.src.is_synced(x.dst)]
 
-    assert len(unsynced_issues) == 0
+#     assert len(unsynced_issues) == 0
 
     # 1. get all gitlab&jira issues (optionally older than specified timestamp)
     # 2. assert that some fields are not equal
@@ -42,24 +42,31 @@ def test_syncs_existing():
     # 5. import old values to the issues
     # 6. update issues to contain old values
 
-    #
-
 
 def test_sync_minimal():
-    # db = MockDatabase()
     gitlab_provider = GitlabProvider()
     jira_provider = JiraProvider()
 
-    data = {"description": "NEW1"}
+    syncer = Syncer(MockDatabase())
+
+    data0 = {"issue_name": "name", "description": "NEW0"}
+    data1 = {"issue_name": "name", "description": "NEW1"}
 
     gitlab_issue = gitlab_provider.get_project_issues("KAN")[0]
-    gitlab_issue.import_values(data)
-    # gitlab_issue.update()
+    gitlab_issue.import_values(data0)
+    gitlab_issue.update()
 
     jira_issue = jira_provider.get_project_issues("KAN")[0]
-    jira_issue.import_values(data)
-    # jira_issue.update()
+    jira_issue.import_values(data1)
+    jira_issue.update()
 
-    # print(gitlab_issue.asdict())
-    # print(jira_issue.asdict())
-    #
+    src_issues = gitlab_provider.get_project_issues("KAN")
+    dst_issues = jira_provider.get_project_issues("KAN")
+
+    syncer.sync_minimal(src_issues, dst_issues)
+    
+    assert all(
+        x.description == y.description
+        for x in src_issues
+        for y in dst_issues
+    )
