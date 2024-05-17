8cb0e51e0b6d6045b1715c7b63fcc385476c1eb9
MasterLogick <31408232+masterlogick@users.noreply.github.com>
2024-05-17 19:23:44 +0300

    fix bugs

--- app/api/endpoints/rules.py
+++ app/api/endpoints/rules.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from fastapi import APIRouter
 from app.core.db import DocumentDatabase
 from app.core.rule import RuleDTO, FieldFilter, RuleDirection
@@ -14,26 +16,29 @@ router = APIRouter(prefix="/api")
 def get_rules(db: DocumentDatabase = Depends(get_db)):
     return crud.get_rules(db)
 
+@dataclass
+class AddRuleRequestDTO:
+    source: FieldFilter
+    direction: str
+    destination: FieldFilter
+
 
 @router.post("/add_rule")
 def add_rule(
-    source: FieldFilter,
-    direction: str,
-    destination: FieldFilter,
-    db: DocumentDatabase = Depends(get_db),
+        req: AddRuleRequestDTO,
+        db: DocumentDatabase = Depends(get_db),
 ):
     dto = RuleDTO(
-        source=source,
-        direction=RuleDirection(direction),
-        destination=destination,
+        source=req.source,
+        direction=RuleDirection(req.direction),
+        destination=req.destination,
     )
-    exit(-1)
+    # exit(-1)
     return crud.add_rule(dto, db)
 
 
 @router.delete("/remove_rule")
 def remove_rule(rule: RuleDTO, db: DocumentDatabase = Depends(get_db)):
     return crud.remove_rule(rule, db)
-
 
 # TODO: error on POSt when rule with  source / dest fields exist
